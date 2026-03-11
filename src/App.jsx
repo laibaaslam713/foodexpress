b783878d739be1554c90f3d0697ed8da5b241ca6
aslamlaiba713 <aslamlaiba713@gmail.com>
2026-03-11 09:59:25 +0500

    update changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import CategoryDisplay from "./components/CategoryDisplay";
 import { foodData } from "./foodData/FoodData"; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element = {<Home/>}/>
+        <Route path='/about' element = {<About/>} />
+        <Route path='/contact' element = {<Contact/>} />
         <Route path="/fastfood/burgers" element={
       <CategoryDisplay 
         title={foodData.fastfood.burgers.title} 
---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../App.css"
+
+const About = () => {
+  return (
+    <div className="about-section">
+        <div className="about-text">
+            <h2>About Food Express</h2>
+            <p>
+            Welcome to Food Express,<br/>
+            your go-to online platform for delicious and freshly prepared food.<br/>
+            We aim to provide a simple and convenient way for customers to explore<br/>
+            our menu and place orders quickly through WhatsApp.<br/>
+            Our focus is on delivering a smooth browsing experience<br/>
+            with an attractive and user-friendly design.<br/>
+            Food Express is built to make online food ordering<br/>
+            easy, fast, and enjoyable.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
